5dc4d0f2cd09bd8fe4feeb7991153052bd74be5c
Mercy Bhakta <mercybhakta@yahoo.com>
2018-09-23 21:46:54 -0700

    Automating input to loop in main

--- benchmark.cpp
+++ benchmark.cpp
@@ -2,21 +2,27 @@
 #include <string>
 #include <stdlib.h>
 #include <chrono>
+
 using namespace std;
 using namespace std::chrono;
+
+//returns the char of the <buffer_string> at index <val>
 char access_val(string buffer_string,unsigned int val)
 {
         return buffer_string.at(val);
 }
+
+//returns a random value
 unsigned int return_rand(unsigned int upper_limit)
 {
         return rand()%upper_limit;
 }
 
-int main ()
+//loops and randomly populates a buffer of <buffer_size>
+//then measures the latency of accessing multiple bytes
+int run_benchmark(unsigned int buffer_size)
 {
-        unsigned int buffer_size;
-        cin >> buffer_size;//User input buffer size
+
         //string str1 = "abcdefghijklmnopqrstuvwxyz";
         string buffer(buffer_size, 'a');//Create a string of bufferlength of chars
         srand(time(NULL));//Seed rand
@@ -38,8 +44,18 @@ int main ()
         steady_clock::time_point t2 = steady_clock::now();
         duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
         long double mean_time = time_span.count() / 1000;
-        cout << mean_time << '\n';
-        //be sure to time a for loop of the rand_array and subtract from time
-        //also, automate
+        cout << buffer_size << "\t\t" << mean_time << '\n';
         return 0;
 }
+
+int main ()
+{
+        int buffer_size = 1024;
+        while (buffer_size <= 67108864)
+        {
+                run_benchmark(buffer_size);
+                buffer_size *= 2;
+        }
+        return 0;
+}
+
