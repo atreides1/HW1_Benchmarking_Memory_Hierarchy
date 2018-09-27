85c8cc86ac9d664b2b16d0787e964beda1461968
Mercy Bhakta <mercybhakta@yahoo.com>
2018-09-26 19:44:49 -0700

    Fixed timing bug so it's in nanoseconds

--- benchmark.cpp
+++ benchmark.cpp
@@ -9,7 +9,7 @@ using namespace std::chrono;
 //returns the char of the <buffer_string> at index <val>
 char access_val(string buffer_string,unsigned int val)
 {
-        return buffer_string.at(val);
+        return buffer_string[val];
 }
 
 //returns a random value
@@ -22,39 +22,41 @@ unsigned int return_rand(unsigned int upper_limit)
 //then measures the latency of accessing multiple bytes
 int run_benchmark(unsigned int buffer_size)
 {
+        struct timespec t0, t1, t2, t3;
+        const int NSECS_IN_SEC = 1000000000;
 
         string buffer(buffer_size, 'a');//Create a string of bufferlength of chars
         srand(time(NULL));//Seed rand
 
-        unsigned int rand_array [1000];
-        for (int j= 0; j<1000; j++)
+        unsigned int rand_array [100000];
+        for (int j= 0; j<100000; j++)
         {
                 rand_array[j]= return_rand(buffer_size);
         }
 
 
         //time this
-        steady_clock::time_point t1 = steady_clock::now();
-                for (unsigned int i = 0; i<1000; i++) //access a part of memory #accesses times
+        clock_gettime(CLOCK_MONOTONIC, &t0);
+                for (unsigned int i = 0; i<100000; i++) //access a part of memory #accesses times
                 {
                         access_val(buffer, rand_array[i]);
                 }
         //time end. mean time = div by iters.
-        steady_clock::time_point t2 = steady_clock::now();
-        duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
-        long double mean_time = time_span.count() / 1000;
+        clock_gettime(CLOCK_MONOTONIC, &t1);
+        const double time_span = (NSECS_IN_SEC * (t1.tv_sec - t0.tv_sec) + (t1.tv_nsec - t0.tv_nsec)) / (double) 100000;
+        long double mean_time = time_span / 100000;
 
-        steady_clock::time_point t3 = steady_clock::now();
-                for (unsigned int i = 0; i<1000; i++) //access a part of memory #accesses times
+        clock_gettime(CLOCK_MONOTONIC, &t2);
+                for (unsigned int i = 0; i<100000; i++) //access a part of memory #accesses times
                 {
                         unsigned int testing_time = rand_array[i]; //Just need to access values - Throws error, but most accurate timing
                 }
 
-        steady_clock::time_point t4 = steady_clock::now();
-        duration<double> rand_array_time = duration_cast<duration<double>>(t4 - t3);
+        clock_gettime(CLOCK_MONOTONIC, &t3);
+        const double rand_array_time = (NSECS_IN_SEC * (t3.tv_sec - t2.tv_sec) + (t3.tv_nsec - t2.tv_nsec)) / (double) 100000;
         //Subtract time to accesss vals consecutively in array/iters
-        long double adjusted_time = (time_span.count() - rand_array_time.count()) / 1000;
-        
+        long double adjusted_time = (time_span - rand_array_time) / 1000;
+
         cout << buffer_size << "\t\t\t" << mean_time << "\t\t" <<adjusted_time << '\n';
 
         return 0;
@@ -71,4 +73,3 @@ int main ()
         }
         return 0;
 }
-
