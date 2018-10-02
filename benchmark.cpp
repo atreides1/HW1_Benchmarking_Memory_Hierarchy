33321014c4d777f208de806f3fc8cc3f3bb729e9
sschlott <43354996+sschlott@users.noreply.github.com>
2018-10-01 19:30:30 -0700

    Added more runs for testing purposes.

--- benchmark.cpp
+++ benchmark.cpp
@@ -7,7 +7,7 @@ using namespace std;
 using namespace std::chrono;
 
 //Getting random values so we can choose random access locations in buffer string
-unsigned int return_rand(unsigned int upper_limit) 
+unsigned int return_rand(unsigned int upper_limit)
 {
         return rand()%upper_limit;
 }
@@ -16,7 +16,7 @@ unsigned int return_rand(unsigned int upper_limit)
 void floodcache()
 {
         const int six_MB = 6000000;
-        string long_string(six_MB, 'm' ); 
+        string long_string(six_MB, 'm' );
         /*On occasion, single character values will be established, used, then posted after a loop
         This is to ensure the compiler does not "optimize" out unused variables.
         The produced column of these printed characters is ignored in our final evaluation of data*/
@@ -38,7 +38,7 @@ int run_benchmark(unsigned int buffer_size)
         //Need seed to be as random as possible each time so the prefetcher cannot "guess" where we will access next
         srand(time(NULL));
         unsigned int* rand_array = NULL;
-        
+
         //Create and populate an array of random values which will correspond to the locations we access in the buffer string
         rand_array = new unsigned int[buffer_size];
         for (unsigned int j= 0; j<buffer_size; j++)
@@ -65,7 +65,7 @@ int run_benchmark(unsigned int buffer_size)
         for (unsigned int z = 0; z < iters; z++)
         {
 
-                for (unsigned int i = 0; i<buffer_size; i++) 
+                for (unsigned int i = 0; i<buffer_size; i++)
                 {
                         o = buffer[rand_array[i]];
                 }
@@ -79,13 +79,13 @@ int run_benchmark(unsigned int buffer_size)
         //Measure time to iter through rand array since it affects time measured above. Subtract this from time_span to get mean adj time
         unsigned int testing_time;
         clock_gettime(CLOCK_MONOTONIC_RAW, &t2);
-                for (unsigned int i = 0; i<buffer_size; i++) 
+                for (unsigned int i = 0; i<buffer_size; i++)
                 {
-                        testing_time = rand_array[i]; 
+                        testing_time = rand_array[i];
                 }
 
         clock_gettime(CLOCK_MONOTONIC_RAW, &t3);
-        cout << testing_time << '\t';
+        cout << testing_time%2 << '\t';
         const double rand_array_time = (NSECS_IN_SEC * (t3.tv_sec - t2.tv_sec) + (t3.tv_nsec - t2.tv_nsec)) / (double) buffer_size;
 
         long double adjusted_time = time_span - (iters * rand_array_time);
@@ -103,6 +103,8 @@ int main ()
         while (buffer_size <= 67108864)
         {
 
+                run_benchmark(buffer_size);
+                run_benchmark(buffer_size);
                 run_benchmark(buffer_size);
                 buffer_size *= 2;
         }
